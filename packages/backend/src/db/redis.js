623857fc29d24e4fd92a1314d0b24455a33aff89
sirsona <swaleh489@gmail.com>
2026-07-11 12:20:18 +0300

    chore: install ioredis and connect to local Redis

---
+++ packages/backend/src/db/redis.js
@@ -0,0 +1,7 @@
+const Redis = require("ioredis");
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+redis.on("error", (err) => console.error("Redis error"));
+
+module.exports = redis;
